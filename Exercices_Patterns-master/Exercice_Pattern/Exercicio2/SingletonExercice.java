cb820b83af1badb17ab1e9ec905c5b07470470ee
GiovannaSelihevic <36748070+GiovannaSelihevic@users.noreply.github.com>
2018-05-08 19:38:55 -0300

    Initial commit

---
+++ Exercices_Patterns-master/Exercice_Pattern/Exercicio2/SingletonExercice.java
@@ -0,0 +1,16 @@
+package Exercicio2;
+
+/**
+ * 
+ * @author Giovanna Selihevic Marques - 81613657
+ *
+ */
+
+public class SingletonExercice {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
